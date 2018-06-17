399d9ea07da183fb988735a38fc0296cffd659b1
Rogerio Barrucalle <rogerio.137@bol.com.br>
2018-06-17 12:59:14 -0300

    Inserindo pedido

---
+++ src/main/java/com/nelioalves/cursomc/services/BoletoService.java
@@ -0,0 +1,21 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+	
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instateDoPedido) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instateDoPedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(cal.getTime());
+		
+	}
+
+}
