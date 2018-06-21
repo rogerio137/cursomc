28ef61bbf1e302d8db3aaeaafa18a9636001e7bf
Rogerio Barrucalle <rogerio.137@bol.com.br>
2018-06-21 20:46:37 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method

--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -40,6 +40,9 @@ public class PedidoService {
 	@Autowired
 	private ClienteService clienteService;
 	
+	@Autowired
+	private EmailService emailService;
+	
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
 		
@@ -71,7 +74,7 @@ public class PedidoService {
 		
 		itemPedidoRepository.save(obj.getItens());
 		
-		System.out.println(obj);
+		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
 
