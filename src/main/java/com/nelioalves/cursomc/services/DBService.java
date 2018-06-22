0bd8a17e7d2fe6a186531c30b5d9657572739dc6
Rogerio Barrucalle <rogerio.137@bol.com.br>
2018-06-21 21:55:29 -0300

    Implementando SmtpEmailService com Servidor Google

--- src/main/java/com/nelioalves/cursomc/services/DBService.java
+++ src/main/java/com/nelioalves/cursomc/services/DBService.java
@@ -119,7 +119,7 @@ public class DBService {
 		estadoRepositiry.save(Arrays.asList(est1, est2));
 		cidadeRepositiry.save(Arrays.asList(c1,c2,c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva","maria@gmail.com","36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva","rogerio.137@bol.com.br","36378912377", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		
 		
