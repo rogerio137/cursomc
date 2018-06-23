890284024bd703d7773c44a1deca7063192075af
Rogerio Barrucalle <rogerio.137@bol.com.br>
2018-06-23 12:53:14 -0300

    Adicionando senha a Cliente

--- src/main/java/com/nelioalves/cursomc/services/DBService.java
+++ src/main/java/com/nelioalves/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
@@ -59,6 +60,9 @@ public class DBService {
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	
 	public void instantiateTestDatabase() throws ParseException {
 		Categoria cat1 = new Categoria(null,"Informática");
@@ -119,7 +123,7 @@ public class DBService {
 		estadoRepositiry.save(Arrays.asList(est1, est2));
 		cidadeRepositiry.save(Arrays.asList(c1,c2,c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva","rogerio.137@bol.com.br","36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva","rogerio.137@bol.com.br","36378912377", TipoCliente.PESSOAFISICA,pe.encode("123") );
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		
 		
