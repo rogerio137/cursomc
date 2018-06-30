d3b70dedab56b579d729fc9927a8de859713dac7
Rogerio Barrucalle <rogerio.137@bol.com.br>
2018-06-30 12:48:49 -0300

    Esqueci a senha

--- src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
@@ -7,13 +7,13 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -73,6 +73,22 @@ public abstract class AbstractEmailService implements EmailService {
 
 		return mimeMessage;
 
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha ");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: "+ newPass);
+		return sm;
 	}
 
 }
