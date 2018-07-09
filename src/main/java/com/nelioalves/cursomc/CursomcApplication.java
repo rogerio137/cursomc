b08b4fee040310e6f6b626dff392979cc38c7581
Rogerio Barrucalle <rogerio.137@bol.com.br>
2018-07-09 10:53:43 -0300

    Enviando imagem via endpoint

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -19,7 +19,7 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("D:\\Imagens\\suf.jpg");
+		
 	}
 }
 
